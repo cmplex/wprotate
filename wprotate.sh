5846d4bb51b31def2e7ce1a86524319f9fb17501
Cedric Haase <cedrichaase@gmail.com>
2014-07-28 21:18:36 +0200

    Eliminate repetitions

--- wprotate.sh
+++ wprotate.sh
@@ -4,8 +4,29 @@
 
 WP_DIR="${HOME}/img/wallpapers/"
 
-NO_OF_WALLPAPERS=$(ls -1 ${WP_DIR} | wc -l)
-RND=$(shuf -i 1-${NO_OF_WALLPAPERS} -n 1)
-RND_FILE=$(ls -1 ${WP_DIR} | sed "${RND}q;d")
+# find unused files, store in $UNUSED
+for file in $(find ${WP_DIR} -type f); do
+  attr -g used ${file} > /dev/null 2>&1 || {
+    UNUSED+=${file}$'\n'
+    ((UNUSED_COUNT++))
+  }
+done
 
-feh --bg-fill "${WP_DIR}/black.png" "${WP_DIR}/${RND_FILE}"
+# if all wallpapers are used, remove used flag from all
+if test -z ${UNUSED_COUNT}; then
+  for file in $(find ${WP_DIR} -type f); do
+    attr -r used ${file}
+  done
+
+  UNUSED=$(find ${WP_DIR} -type f)
+  UNUSED_COUNT=$(ls -1 ${WP_DIR} | wc -l)
+fi
+
+
+# get a random image file from UNUSED files
+RND=$(shuf -i 1-${UNUSED_COUNT} -n 1)
+RND_FILE=$(printf '%s\n' ${UNUSED} | sed "${RND}q;d")
+
+
+feh --bg-fill "${WP_DIR}/black.png" "${RND_FILE}"
+attr -s used -V 1 "${RND_FILE}" > /dev/null 2>&1
