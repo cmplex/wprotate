59f4098db8b6d5da2dda2634b81e414f24b58ad7
Cedric Haase <cedrichaase@gmail.com>
2015-11-27 20:08:17 +0100

    Add help page and slim wallpaper setting

--- wprotate.sh
+++ wprotate.sh
@@ -4,6 +4,27 @@
 
 WP_DIR="${HOME}/img/wallpapers/"
 
+case ${1} in
+  -r|--remove)
+    echo "$(date) removing previous wallpaper" >> /var/log/wprotate.log
+    sudo rm $(cat /var/local/wprotate/wallpaper)
+    ;;
+  -h|--help)
+    echo "wprotate"
+    echo "chooses and sets a random wallpaper from your wallpaper-directory"
+    echo
+    echo "Usage:"
+    echo "wprotate [OPTION]"
+    echo
+    echo "--remove | -r"
+    echo "  remove currently active wallpaper before switching"
+    echo "--help | -h"
+    echo "  show this page"
+    exit 0
+    ;;
+esac
+
+
 # find unused files, store in $UNUSED
 for file in $(find ${WP_DIR} -type f); do
   attr -g used ${file} > /dev/null 2>&1 || {
@@ -27,6 +48,11 @@ fi
 RND=$(shuf -i 1-${UNUSED_COUNT} -n 1)
 RND_FILE=$(printf '%s\n' ${UNUSED} | sed "${RND}q;d")
 
+echo ${RND_FILE} > /var/local/wprotate/wallpaper
 
-feh --bg-fill "${WP_DIR}/black.png" "${RND_FILE}"
+# set wallpaper as slim background image
+sudo rm /usr/share/slim/themes/default/background.jpg
+sudo ln -s ${RND_FILE} /usr/share/slim/themes/default/background.jpg
+
+feh --bg-fill "${RND_FILE}"
 attr -s used -V 1 "${RND_FILE}" > /dev/null 2>&1
